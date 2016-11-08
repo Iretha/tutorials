cdf2708ab227bccf1ac43c5712bbab6db7522873
Iretha <milena.bd@gmail.com>
2016-11-08 20:25:22 +0200

    cleanup

---
+++ example-di-spring/src/main/java/com/smdev/spring/msg/MessageAppException.java
@@ -0,0 +1,12 @@
+package com.smdev.spring.msg;
+
+/**
+ * Exception thrown by the application
+ *
+ * @author Ireth
+ */
+public class MessageAppException extends Exception {
+
+	/** */
+	private static final long serialVersionUID = -7744038993937833509L;
+}
