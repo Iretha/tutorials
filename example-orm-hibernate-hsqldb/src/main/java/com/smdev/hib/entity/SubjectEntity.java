400ba7fe9fcc7b04a1688f62d6628d50e185aa82
Iretha <milena.bd@gmail.com>
2016-11-15 18:56:56 +0200

    orphanRemoval added

--- example-orm-hibernate-hsqldb/src/main/java/com/smdev/hib/entity/SubjectEntity.java
+++ example-orm-hibernate-hsqldb/src/main/java/com/smdev/hib/entity/SubjectEntity.java
@@ -2,7 +2,6 @@ package com.smdev.hib.entity;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -29,7 +28,7 @@ public class SubjectEntity implements DBEntry {
 	/** */
 	private static final long serialVersionUID = 258750302448670999L;
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "subject")
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "subject", orphanRemoval = true)
 	private List<CourseEntity> courses;
 
 	@Id
